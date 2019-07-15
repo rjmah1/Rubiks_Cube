4347c44bd8388a9f09bf571009913601318d793f
rjmah1 <rjmah1@ualberta.ca>
2019-07-15 00:34:21 -0600

    added the basis for scramble

---
+++ scramble.py
@@ -0,0 +1,14 @@
+import random
+
+# create an enum/list to choose values from ie U D L R F B
+
+# function to get a random number from 18 to 25 as the length of the scramble
+
+# function to poll a random number between the possible move sets
+
+# if the number corresponds with the same move as previous ie U with U' or U2
+# then the value is discarded and a new number is taken
+
+# randomize the seed.
+random.seed()
+
